d641a180409b3c032fa34c5fc25664743e49b517
C-YooJin <ioi_3312@naver.com>
2020-06-24 16:34:34 +0900

    Add Exceptions Handling function

--- kafka/kafka_consumer_python.py
+++ kafka/kafka_consumer_python.py
@@ -6,14 +6,26 @@ consumer = KafkaConsumer('topic-name',
                          auto_offset_reset = 'earliest',
                          enable_auto_commit = True)
 
-for message in consumer:
-    print(" ")
-    print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                         message.offset, message.key,
-                                         message.value))
-    print(" ")
-    print("%s" % (message.value))
+def is_json(input):
+    try:
+        result_json = json.loads(input)
+    except ValueError as e:
+        return False
+    return result_json
 
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: 'message.value.decode('utf-8')'
+    print(" ")
+    input = message.value.decode("utf-8")
+    result = is_json(input)
+
+    if result["BasicLog"]["PCID"] == "blahblah":
+        print("Topic: %s" % message.topic)
+        print("Partition: %d" % message.partition)
+        print("Offset: %d" % message.offset)
+        print("Key: %s" % message.key)
+        print(result)
 
 """
 KafkaConsumer(value_deserializer=msgpack.unpackb)
